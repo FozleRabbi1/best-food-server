c9ce40037175abbe5b91e8bf9b9ecfe55a0faf66
Fozle Rabbi <fozlerabbi9790@gmail.com>
2023-05-03 03:46:02 +0600

    some modification and deploy serve in versel

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ app.get("/allFoods/:name", (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port  ${port}`)
+  console.log(`Example app listening port =  ${port}`)
 })  
