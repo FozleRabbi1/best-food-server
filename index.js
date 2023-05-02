30b489ed3fdcf3500b1c21bcd39483a8376fc258
Fozle Rabbi <fozlerabbi9790@gmail.com>
2023-05-02 15:26:30 +0600

    added json data and load

--- index.js
+++ index.js
@@ -16,18 +16,25 @@ app.get("/chef", (req, res) => {
   res.send(chefData)
 })
 
-app.get("/allFoods", (req, res) => {
-  res.send(allFoods)
-})
 
 app.get("/chef/:name", (req, res) => {
   const name = req.params.name;
   const modifyName = name.replace(/\s+/g, '');
-
   const getData = chefData.find(data => data.ChefName.replace(/\s+/g, '') == modifyName)
-
   // console.log(modifyName)
   res.send(getData)
+})
+
+app.get("/allFoods", (req, res) => {
+  res.send(allFoods)
+})
+
+
+app.get("/allFoods/:name", (req, res) => {
+  const name = req.params.name;
+  const modifyName = name.replace(/\s+/g, '');
+  const filterData = allFoods.filter(data => data.name.replace(/\s+/g, '') == modifyName)
+  res.send(filterData)
 })
 
 app.listen(port, () => {
