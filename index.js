24f14484738cc1c880f45130bea0fc3ea5a5919a
Fozle Rabbi <fozlerabbi9790@gmail.com>
2023-05-02 15:02:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const cors = require('cors')
+
+app.use(cors());
+
+const chefData = require("./datas/chef.json")
+const allFoods = require("./datas/allFoos.json")
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/chef", (req, res) => {
+  res.send(chefData)
+})
+
+app.get("/allFoods", (req, res) => {
+  res.send(allFoods)
+})
+
+app.get("/chef/:name", (req, res) => {
+  const name = req.params.name;
+  const modifyName = name.replace(/\s+/g, '');
+
+  const getData = chefData.find(data => data.ChefName.replace(/\s+/g, '') == modifyName)
+
+  // console.log(modifyName)
+  res.send(getData)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
